ba81456d3255e75e9054d5cc3df90de7f302fb76
Edara2004 <eduarrodriguez2004@hotmail.com>
2025-06-22 00:01:40 -0400

    Connected successfully Database with login, adding new content for the next week, I'll probably add the first movies.

--- config/config.php
+++ config/config.php
@@ -14,8 +14,7 @@
                 $options = [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_EMULATE_PREPARES => false,
-
+                    PDO::ATTR_EMULATE_PREPARES => false
                 ];
                 return new PDO($db, $this->db_user, $this->db_password, $options);
             } catch(\Throwable $th){
--- src/Controllers/Registration.php
+++ src/Controllers/Registration.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $email = $_POST['email-user'];
     $numeroTelefono = $_POST['numero-de-telefono'];
     $user = $_POST['user'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $passwords = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
     $nombreCompleto = $nombre . " " . $apellido;
 
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $stmtinsert = $pdo-> prepare($sql);
         $stmtinsert->execute([
                 $user,
-                $password,
+                $passwords,
                 $nombreCompleto,
                 $email,
                 $numeroTelefono,
