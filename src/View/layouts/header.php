61f460293188ad33f7d95f1d0fd0faaf8dc7d0e2
Edara2004 <eduarrodriguez2004@hotmail.com>
2025-06-29 14:09:07 -0400

    Responsives registration and login, refactoring code, fixing some parameter fixes for a better user experience.

--- src/View/layouts/header.php
+++ src/View/layouts/header.php
@@ -1,14 +1,34 @@
 <header>
-    <div class="bg-gray-800">
-        <div class="mx-auto w-full max-w-screen-xl mb-6 py-5 p-6 columns-2">
-            <div class="place-items-start">
-                <a href="/index.php" class="font-bold text-xl text-white">Loopkin</a>
-            </div>
-            <div class="text-end">
+    <nav class="bg-gray-800 border-gray-200 dark:bg-gray-800">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <span class="font-bold text-xl text-white whitespace-nowrap dark:text-white">Loopkin</span>
+            </a>
+
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <a href="../login/login.php"
-                class="text-gray-800 transition duration-350 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-500 focus:outline-none dark:focus:ring-gray-800">Ingresar</a>
-                <a href="/proyecto-universidad-tailwind/src/View/login/registration.php" class="text-white transition duration-350 bg-gray-700 hover:bg-gray-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-gray-600 dark:hover:bg-gray-400 focus:outline-none dark:focus:ring-gray-800">Registrate</a>
+                    class="text-gray-800 transition duration-350 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-500 focus:outline-none dark:focus:ring-gray-800 hidden md:inline-block">Ingresar</a>
+                <a href="/proyecto-universidad-tailwind/src/View/login/registration.php"
+                    class="text-white transition duration-350 bg-gray-700 hover:bg-gray-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-gray-600 dark:hover:bg-gray-400 focus:outline-none dark:focus:ring-gray-800 ml-2 hidden md:inline-block">Registrate</a>
+                
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Abrir menú principal</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-800 dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
+                    <li>
+                        <a href="../login/login.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ingresar</a>
+                    </li>
+                    <li>
+                        <a href="/proyecto-universidad-tailwind/src/View/login/registration.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Registrate</a>
+                    </li>
+                    </ul>
             </div>
         </div>
-    </div>
+    </nav>
 </header>
--- src/View/login/login.php
+++ src/View/login/login.php
@@ -8,57 +8,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 </head>
 
 <body class="bg-gray-800">
 
     <!-- Header -->
 
-    <?php
-    require '../../View/layouts/header.php';
-    ?>
+    <?php require '../layouts/header-login.php' ?>
 
     <main>
         <div>
+            <img src="../../../public/assets/img/background-image.avif" class="absolute inset-0 w-full h-full object-cover brightness-30 z-[-1]">
             <!-- Screen size -->
             <div class="h-screen">
 
                 <div class="text-center">
                     <div class="animate-pulse">
-                        <h1 class="mt-5 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">¡Bienvenidos!</span></h1>
+                        <h1 class="text-transparent mt-5 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+                            <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">¡Bienvenidos!</span>
+                        </h1>
                     </div>
                 </div>
 
                 <!-- Login form with all components -->
                 <div class="flex justify-center place-items-center p-2 pr-4 pt-10 mb-70">
-                    <div class="border border-white bg-white/80 shadow-xl/30 rounded-md w-100">
+                    <div class="border bg-white/70 shadow-xl/90 rounded-md w-auto h-auto">
 
                         <!-- Login Form -->
                         <form action="../../Controllers/user.php" method="post">
-                            <h1 class="text-center font-mono font-extrabold text-4xl py-5">Login</h1>
+                            <h1 class="text-center font-sans font-extrabold text-3xl md:text-4xl py-5 text-gray-700">Iniciar sesion</h1>
 
-                            <!-- User -->
-                            <div class="mb-3">
-                                <div class="mb-3 grid grid-cols-2">
-                                    <div class="col-span-2">
-                                        <label for="userLogin" class="p-2 ml-10 text-center">Usuario</label>
-                                        <input type="text" autocomplete="off" name="username" class="border border-gray-800 rounded-md ps-3 h-7 mr-5">
+                            <div class="px-4 sm:px-6">
+                                <div class="mb-4"> <label for="userLogin" class="block text-gray-700 text-sm font-bold mb-2">Usuario</label>
+                                    <input type="text" autocomplete="off" name="username" id="userLogin"
+                                        class="border border-gray-800 rounded-md ps-3 py-1.5 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        placeholder="Tu nombre de usuario" required>
+                                </div>
+
+                                <div class="mb-6"> <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Contraseña</label>
+                                    <div class="flex items-center"> <input class="border border-gray-800 focus:ring-2 focus:ring-blue-500 rounded-l-md ps-3 py-1.5 w-full"
+                                            type="password" name="password" id="userPasswordId" placeholder="Tu contraseña" required>
+                                        <button class="bg-white/80 border transition duration-350 border-gray-500 px-3 py-2 rounded-r-md hover:bg-gray-800 dark:hover:text-white/80 text-sm flex-shrink-0"
+                                            type="button" id="button-addon2" onclick="togglePasswordLogin()">Mostrar</button>
                                     </div>
                                 </div>
 
-                                <!-- Password -->
-                                <div class="mb-5 grid grid-cols-2">
-                                    <div class="col-span-2">
-                                        <label for="password" class="p-2 ml-3.5">Contraseña</label>
-                                        <input class="border border-gray-800 focus:ring-white focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-md ps-3 h-7" type="password" name="password" id="userPasswordId">
-                                        <button class="bg-white/80 border transition duration-350 border-gray-500 px-3 w-20 rounded-md h-7 hover:bg-gray-800 dark:hover:text-white/80" type="button" id="button-addon2" onclick="togglePasswordLogin()">Mostrar</button>
+                                <!-- Redirecionar al registro de usuario -->
+                                <div>
+                                    <div class="text-center mb-3">
+                                        <label>¿No tienes </label><a href="registration.php" class="text-blue-600 hover:underline dark:text-blue-500">cuenta registrate </a><label>?</label>
                                     </div>
                                 </div>
-
-                                <!-- Enter button -->
+                                
                                 <div class="mb-3 text-center">
-                                    <input class="bg-white/40 transition duration-350 border border-gray-500 w-60 rounded-md h-8 hover:bg-gray-800 dark:hover:text-white" type="submit" value="Ingresar">
+                                    <input class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md w-full sm:w-auto cursor-pointer focus:outline-none focus:shadow-outline"
+                                        type="submit" value="Ingresar">
                                 </div>
+
 
                             </div>
                         </form>
