f7ec4b53228c964f7f36be9adad5904a35c8a9e5
Edara2004 <eduarrodriguez2004@hotmail.com>
2025-07-13 00:13:53 -0400

    Put ID for the movie database

--- src/View/pages/movies/avenger-end-game.php
+++ src/View/pages/movies/avenger-end-game.php
@@ -1,1 +1,25 @@
-<?php session_start() ?>
+<?php 
+session_start();
+require '../../../Services/api-tmdb.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet"/>
+</head>
+<body class="bg-gray-800">
+
+    <div>
+        <p class="text-white text-2xl"><?php echo $title?></p>
+    </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+</html>
